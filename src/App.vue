b82a24e5400f62c3f9939c4868ad2a4eaae63877
Rachit2003 <rachitmody86@gmail.com>
2021-08-18 18:56:56 +0530

    Third Task(To-do-list)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+<body>
+    <div  class="container">
+        <h1>Todo List</h1>
+        <h2>New ToDo</h2>
+        <div>
+            <input type="text" v-model="todo" class="int" >
+            <h4 v-if="isError">Text field required</h4>
+            <input type="submit" value="Add" @click="storeTodo" class="btn">
+        </div>
+        
+        <h3>To-Do List</h3>
+        <hr size="1" width="95%" color="white">
+<div >
+        <ul>
+            <li v-for="(todo, index) in todos" :key=index>
+                {{ todo }}
+                <button @click="removeTodo(index)" class="edit">Delete</button>
+            </li>
+        </ul>
+        
+    </div>
+    </div>
+</body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+           
+            data() {
+              return {
+                todo: '',
+                todos: [],
+                selectedTodo: null,
+                isError: false
+               
+              }
+            },            
+            methods: {
+               
+                storeTodo() {
+                    
+                    if(this.todo!=""){
+                    this.todos.push(this.todo)
+                    this.todo = ''              
+                    this.isError=false
+                    
+                    }
+
+                    else{
+                        this.isError=true
+                    }
+                    
+                },
+                removeTodo(index) {
+                    this.todos.splice(index, 1)
+                },
+                
+            }
+  
 }
 </script>
 
@@ -19,8 +69,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
